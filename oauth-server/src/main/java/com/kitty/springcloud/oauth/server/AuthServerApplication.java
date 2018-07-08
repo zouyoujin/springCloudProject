b34ac88d5a2dd634bc31acb9bdfce7a4a1fa0aac
zouyoujin <zouyoujin168@gmail.com>
2018-07-08 11:34:17 +0800

    eureka-server 多实例配置及多环境打包配置

--- oauth-server/src/main/java/com/kitty/springcloud/oauth/server/AuthServerApplication.java
+++ oauth-server/src/main/java/com/kitty/springcloud/oauth/server/AuthServerApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableDiscoveryClient
@@ -20,12 +19,6 @@ public class AuthServerApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerApplication.class, args);
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
 	}
 
 }
--- oauth-server/src/main/java/com/kitty/springcloud/oauth/server/config/ResourceServerConfig.java
+++ oauth-server/src/main/java/com/kitty/springcloud/oauth/server/config/ResourceServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  * 
@@ -19,6 +20,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	private static final String AUTH_RESOURCE_ID = "*";
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		   http
@@ -31,4 +34,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
            .and()
            .httpBasic();
 	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.resourceId(AUTH_RESOURCE_ID).stateless(true);
+	}
+	
+	
 }
